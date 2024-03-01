d130563ba15e20cf7830e7a388adad613c6f0457
pockocmoc <pockocmoc83@gmail.com>
2024-03-01 11:34:40 +0300

    initial commit

---
+++ src/test/java/com/pockcomoc/HelloControllerIT.java
@@ -0,0 +1,20 @@
+package com.pockcomoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloControllerIT {
+	@Autowired
+	private TestRestTemplate template;
+
+	@Test
+	void getHello() throws Exception {
+		ResponseEntity<String> response = template.getForEntity("/", String.class);
+		assertThat(response.getBody()).isEqualTo("Greetings from Spring Boot!");
+	}
+}
